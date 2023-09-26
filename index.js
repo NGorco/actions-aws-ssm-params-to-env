43afd55c470d9ff9ccef642c64c4ef5962ccf5a5
Alex <alexeypetlenko@gmail.com>
2023-09-26 15:20:34 -0600

    Update index.js: added comma-separated paths

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const core = require('@actions/core');
 const ssm = require('./ssm-helper');
 
-async function run_action()
-{
-    try
-    {
+async function run_action() {
+    try {
         const ssmPath = core.getInput('ssm-path', { required: true });
         const getChildren = core.getInput('get-children') === 'true';
         const prefix = core.getInput('prefix');
@@ -12,27 +10,27 @@ async function run_action()
         const decryption = core.getInput('decryption') === 'true';
         const maskValues = core.getInput('mask-values') === 'true';
 
-        const params = await ssm.getParameters(ssmPath, getChildren, decryption, region);
-        for (let param of params)
-        {
-            const parsedValue = parseValue(param.Value);
-            if (typeof(parsedValue) === 'object') // Assume JSON object
-            {
-                core.debug(`parsedValue: ${JSON.stringify(parsedValue)}`);
-                // Assume basic JSON structure
-                for (var key in parsedValue)
-                {
-                    setEnvironmentVar(prefix + key, parsedValue[key], maskValues);
+        const paths = ssmPath.split(',');
+
+        for (let path of paths) {
+            const params = await ssm.getParameters(path.trim(), getChildren, decryption, region);
+            for (let param of params) {
+                const parsedValue = parseValue(param.Value);
+                
+                if (typeof(parsedValue) === 'object') { // Assume JSON object  
+                    core.debug(`parsedValue: ${JSON.stringify(parsedValue)}`);
+                    // Assume basic JSON structure
+                    for (var key in parsedValue) {
+                        setEnvironmentVar(prefix + key, parsedValue[key], maskValues);
+                    }
+                } else {
+                    core.debug(`parsedValue: ${parsedValue}`);
+                    // Set environment variable with ssmPath name as the env variable
+                    var split = param.Name.split('/');
+                    var envVarName = prefix + split[split.length - 1];
+                    core.debug(`Using prefix + end of ssmPath for env var name: ${envVarName}`);
+                    setEnvironmentVar(envVarName, parsedValue, maskValues);
                 }
-            }
-            else
-            {
-                core.debug(`parsedValue: ${parsedValue}`);
-                // Set environment variable with ssmPath name as the env variable
-                var split = param.Name.split('/');
-                var envVarName = prefix + split[split.length - 1];
-                core.debug(`Using prefix + end of ssmPath for env var name: ${envVarName}`);
-                setEnvironmentVar(envVarName, parsedValue, maskValues);
             }
         }
     }
@@ -43,21 +41,16 @@ async function run_action()
 }
 
 
-function parseValue(val)
-{
-    try
-    {
+function parseValue(val) {
+    try {
         return JSON.parse(val);
-    }
-    catch
-    {
+    } catch {
         core.debug('JSON parse failed - assuming parameter is to be taken as a string literal');
         return val;
     }
 }
 
-function setEnvironmentVar(key, value, maskValue)
-{
+function setEnvironmentVar(key, value, maskValue) {
     if (maskValue) {
         core.setSecret(value);
     }
