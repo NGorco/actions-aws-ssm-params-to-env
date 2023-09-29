5f21ddd8e1d81309b82025ff78240c4a5633a3b2
Alex <alexeypetlenko@gmail.com>
2023-09-29 17:41:24 -0600

    Update ssm-helper.js

--- ssm-helper.js
+++ ssm-helper.js
@@ -1,29 +1,40 @@
-const { SSMClient, GetParametersCommand, GetParametersByPathCommand } = require("@aws-sdk/client-ssm");
+const {
+    SSMClient,
+    GetParametersCommand,
+    GetParametersByPathCommand
+} = require("@aws-sdk/client-ssm");
 
 const getParameters = async (ssmPath, getChildren, decryption, region) => {
-    const client = new SSMClient({ region: region });
-    let promise;
-    
-    const subProcess = require('child_process')
-    subProcess.exec('aws ssm get-parameters-by-path --path /env/test-be', (err, stdout, stderr) => {
-      if (err) {
-        console.error(err)
-        process.exit(1)
-      } else {
-        console.log(`The stdout Buffer from shell: ${stdout.toString()}`)
-        console.log(`The stderr Buffer from shell: ${stderr.toString()}`)
-      }
+    const client = new SSMClient({
+        region: region
     });
+    let promise;
 
     if (getChildren) {
-        const input = { // GetParametersByPathRequest
-            Path: ssmPath,
-            WithDecryption: decryption
-        };
-        const command = new GetParametersByPathCommand(input);
-        promise = client.send(command);
-    }
-    else {
+        const subProcess = require('child_process');
+        promise = new Promise((resolve, reject) => {
+            subProcess.exec(
+                `aws ssm get-parameters-by-path --path ${ssmPath} ${decryption ? '--decryption': ''}`,
+                (err, stdout, stderr) => {
+                    if (err) {
+                        reject(err);
+                        process.exit(1);
+                    } else {
+                        let params = {
+                            Parameters: []
+                        };
+                        try {
+                            params = JSON.parse(stdout.toString());
+                        } catch (error) {
+                            reject(error);
+                            process.exit(1);
+                        }
+                        resolve(params);
+                    }
+                }
+            );
+        });
+    } else {
         const input = { // GetParametersRequest
             Names: [ // ParameterNameList // required
                 ssmPath,
@@ -37,4 +48,6 @@ const getParameters = async (ssmPath, getChildren, decryption, region) => {
     return response.Parameters;
 }
 
-module.exports = { getParameters };
+module.exports = {
+    getParameters
+};
